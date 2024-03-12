85749dbf9f3dd61935834d5b7d0db8a180b4b272
Boris Sazhin <uniqueue1111@gmail.com>
2024-03-12 18:09:11 +0300

    Initial commit

---
+++ Calculator/src/main/java/vistar/practice/ru/Calculator.java
@@ -0,0 +1,10 @@
+package vistar.practice.ru;
+
+public class Calculator {
+        public int add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+
+
---
+++ CalculatorMain/src/main/java/vistar/practice/ru/Main.java
@@ -0,0 +1,9 @@
+package vistar.practice.ru;
+
+
+public class Main {
+    public static void main(String[] args) {
+            Calculator c = new Calculator();
+        System.out.println(c.add(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+        }
+    }
